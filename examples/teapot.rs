e58a2d2d224638f68ecb2cf970165880024ca636
yazgoo <yazgoo@gmail.com>
2020-01-21 08:37:57 +0100

    :sparkles: First commit

---
+++ examples/teapot.rs
@@ -0,0 +1,118 @@
+use euc::{buffer::Buffer2d, rasterizer, Pipeline, Target};
+use std::path::Path;
+use tobj;
+use vek::*;
+use terminal_size::{Width, Height, terminal_size};
+use blockish::render;
+
+struct Teapot<'a> {
+    mvp: Mat4<f32>,
+    positions: &'a [Vec3<f32>],
+    normals: &'a [Vec3<f32>],
+    light_dir: Vec3<f32>,
+}
+
+impl<'a> Pipeline for Teapot<'a> {
+    type Vertex = u32; // Vertex index
+    type VsOut = Vec3<f32>; // Normal
+    type Pixel = u32; // BGRA
+
+    #[inline(always)]
+    fn vert(&self, v_index: &Self::Vertex) -> ([f32; 4], Self::VsOut) {
+        let v_index = *v_index as usize;
+        // Find vertex position
+        let v_pos = self.positions[v_index] + Vec3::new(0.0, -0.5, 0.0); // Offset to center the teapot
+        (
+            // Calculate vertex position in camera space
+            (self.mvp * Vec4::from_point(v_pos)).into_array(),
+            // Find vertex normal
+            self.normals[v_index],
+        )
+    }
+
+    #[inline(always)]
+    fn frag(&self, norm: &Self::VsOut) -> Self::Pixel {
+        let ambient = 0.2;
+        let diffuse = norm.dot(self.light_dir).max(0.0) * 0.5;
+        let specular = self
+            .light_dir
+            .reflected(Vec3::from(self.mvp * Vec4::from(*norm)).normalized())
+            .dot(-Vec3::unit_z())
+            .powf(20.0);
+
+        let light = ambient + diffuse + specular;
+        let color = (Rgba::new(1.0, 0.7, 0.1, 1.0) * light).clamped(Rgba::zero(), Rgba::one());
+
+        let bytes = (color * 255.0).map(|e| e as u8).into_array();
+        (bytes[2] as u32) << 0
+            | (bytes[1] as u32) << 8
+            | (bytes[0] as u32) << 16
+            | (bytes[3] as u32) << 24
+    }
+}
+
+fn main() {
+    // print!("\x1b[2J");
+    let size = terminal_size();
+if let Some((Width(w), Height(h))) = size {
+    let _w = (w * 8 - 1) as usize;
+    let _h = (h * 16 / 2 - 1) as usize;
+    let mut color = Buffer2d::new([_w, _h], 0);
+    let mut depth = Buffer2d::new([_w, _h], 1.0);
+
+    let obj = tobj::load_obj(&Path::new("examples/data/teapot.obj")).unwrap();
+    let indices = &obj.0[0].mesh.indices;
+    let positions = obj.0[0]
+        .mesh
+        .positions
+        .chunks(3)
+        .map(|sl| Vec3::from_slice(sl))
+        .collect::<Vec<_>>();
+    let normals = obj.0[0]
+        .mesh
+        .normals
+        .chunks(3)
+        .map(|sl| Vec3::from_slice(sl))
+        .collect::<Vec<_>>();
+
+    for i in 0.. {
+        let mvp = Mat4::perspective_fov_rh_no(1.3, _w as f32, _h as f32, 0.01, 100.0)
+            * Mat4::translation_3d(Vec3::new(0.0, 0.0, -1.5))
+            * Mat4::<f32>::scaling_3d(0.8)
+            * Mat4::rotation_x((i as f32 * 0.002).sin() * 8.0)
+            * Mat4::rotation_y((i as f32 * 0.004).cos() * 4.0)
+            * Mat4::rotation_z((i as f32 * 0.008).sin() * 2.0);
+
+        color.clear(0);
+        depth.clear(1.0);
+
+        Teapot {
+            mvp,
+            positions: &positions,
+            normals: &normals,
+            light_dir: Vec3::new(1.0, 1.0, 1.0).normalized(),
+        }
+        .draw::<rasterizer::Triangles<_>, _>(indices, &mut color, &mut depth);
+
+    let raw_slice = color.as_ref();
+    let width = _w as u32;
+    let height = _h as u32;
+    render(width, height as u32, &|x, y| {
+        let start = (((height - 1 - y) * width + x)) as usize;
+        let colors = raw_slice[start];
+         ((colors >> 16 & 0xff) as u8, (colors >> 8 & 0xff) as u8, ( colors & 0xff) as u8)
+    });
+
+    /*
+        if win.is_open() {
+            win.update_with_buffer(color.as_ref()).unwrap();
+        } else {
+            break;
+        }
+    */
+    }
+} else {
+    println!("Unable to get terminal size");
+}
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,184 @@
+use std::collections::HashMap;
+use std::io::{self, Write};
+
+#[inline(always)]
+fn bit_count(x: u32) -> usize {
+/* first let res = x&0xAAAAAAAA >> 1 + x&55555555
+ * after that the (2k)th and (2k+1)th bits of the res
+ * will be the number of 1s that contained by the (2k)th 
+ * and (2k+1)th bits of x
+ * we can use a similar way to caculate the number of 1s
+ * that contained by the (4k)th and (4k+1)th and (4k+2)th 
+ * and (4k+3)th bits of x, so as 8, 16, 32
+ */ 
+    let mut var_a = (85 << 8) | 85;
+    var_a = (var_a << 16) | var_a;
+    let mut res = ((x>>1) & var_a) + (x & var_a);
+
+    var_a = (51 << 8) | 51;
+    var_a = (var_a << 16) | var_a;
+    res = ((res>>2) & var_a) + (res & var_a);
+
+    var_a = (15 << 8) | 15;
+    var_a = (var_a << 16) | var_a;
+    res = ((res>>4) & var_a) + (res & var_a);
+
+    var_a = (255 << 16) | 255;
+    res = ((res>>8) & var_a) + (res & var_a);
+
+    var_a = (255 << 8) | 255;
+    res = ((res>>16) & var_a) + (res & var_a);
+    return res as usize;
+}
+
+fn find_closest_group(groups: &Vec<u64>, group: u64) -> u64 {
+    let mut min : u64 = 0;
+    let mut min_distance = std::usize::MAX;
+    for template in groups {
+        let xor = template ^ group;
+        let distance = bit_count((xor >> 32) as u32) + bit_count((xor & 0xffffffff) as u32);
+        if distance < min_distance {
+            min_distance = distance;
+            min = *template;
+        }
+        if distance == 1 { return min }
+    }
+    min
+}
+
+#[inline(always)]
+fn grey_scale(rgba: &(u8, u8, u8)) -> usize {
+    ((rgba.0 as usize + rgba.1 as usize + rgba.2 as usize) / 3)
+}
+
+pub fn render(width: u32, height: u32, coordinate_to_rgb: &dyn Fn(u32, u32) -> (u8, u8, u8)) {
+    let mut transforms = HashMap::new();
+    transforms.insert(u64::from_str_radix(&("0".repeat(8*3)+&"1".repeat(8)+&"0".repeat(8*4)).as_str(), 2).unwrap(), (true, "─"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(4)+&"1".repeat(8)+&"0".repeat(8).repeat(3)).as_str(), 2).unwrap(), (true, "─"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(3)+&"1".repeat(8).repeat(2)+&"0".repeat(8).repeat(3)).as_str(), 2).unwrap(), (true, "━"));
+    transforms.insert(u64::from_str_radix(&("00010000".repeat(8)).as_str(), 2).unwrap(), (true, "│"));
+    transforms.insert(u64::from_str_radix(&("00001000".repeat(8)).as_str(), 2).unwrap(), (true, "│"));
+    transforms.insert(u64::from_str_radix(&("00011000".repeat(8)).as_str(), 2).unwrap(), (true, "┃"));
+
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(3)+&"0".repeat(8)+&"1".repeat(8).repeat(4)).as_str(), 2).unwrap(), (false, "─"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(4)+&"0".repeat(8)+&"1".repeat(8).repeat(3)).as_str(), 2).unwrap(), (false, "─"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(3)+&"0".repeat(8).repeat(2)+&"1".repeat(8).repeat(3)).as_str(), 2).unwrap(), (false, "━"));
+    transforms.insert(u64::from_str_radix(&("11101111".repeat(8)).as_str(), 2).unwrap(), (false, "│"));
+    transforms.insert(u64::from_str_radix(&("11110111".repeat(8)).as_str(), 2).unwrap(), (false, "│"));
+    transforms.insert(u64::from_str_radix(&("11100111".repeat(8)).as_str(), 2).unwrap(), (false, "┃"));
+
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(4)+&"0".repeat(8).repeat(4)).as_str(), 2).unwrap(), (true, "▀"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(7)+&"1".repeat(8)).as_str(), 2).unwrap(), (true, "▁"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(6)+&"1".repeat(8).repeat(2)).as_str(), 2).unwrap(), (true, "▂"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(5)+&"1".repeat(8).repeat(3)).as_str(), 2).unwrap(), (true, "▃"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(4)+&"1".repeat(8).repeat(4)).as_str(), 2).unwrap(), (true, "▄"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(3)+&"1".repeat(8).repeat(5)).as_str(), 2).unwrap(), (true, "▅"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(2)+&"1".repeat(8).repeat(6)).as_str(), 2).unwrap(), (true, "▆"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(1)+&"1".repeat(8).repeat(7)).as_str(), 2).unwrap(), (true, "▇"));
+
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(4)+&"1".repeat(8).repeat(4)).as_str(), 2).unwrap(), (false, "▀"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(7)+&"0".repeat(8)).as_str(), 2).unwrap(), (false, "▁"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(6)+&"0".repeat(8).repeat(2)).as_str(), 2).unwrap(), (false, "▂"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(5)+&"0".repeat(8).repeat(3)).as_str(), 2).unwrap(), (false, "▃"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(4)+&"0".repeat(8).repeat(4)).as_str(), 2).unwrap(), (false, "▄"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(3)+&"0".repeat(8).repeat(5)).as_str(), 2).unwrap(), (false, "▅"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(2)+&"0".repeat(8).repeat(6)).as_str(), 2).unwrap(), (false, "▆"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(1)+&"0".repeat(8).repeat(7)).as_str(), 2).unwrap(), (false, "▇"));
+
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(8)).as_str(), 2).unwrap(), (true, "█"));
+    transforms.insert(u64::from_str_radix(&("11111110".repeat(8)).as_str(), 2).unwrap(), (true, "▉"));
+    transforms.insert(u64::from_str_radix(&("11111100".repeat(8)).as_str(), 2).unwrap(), (true, "▊"));
+    transforms.insert(u64::from_str_radix(&("11111000".repeat(8)).as_str(), 2).unwrap(), (true, "▋"));
+    transforms.insert(u64::from_str_radix(&("11110000".repeat(8)).as_str(), 2).unwrap(), (true, "▌"));
+    transforms.insert(u64::from_str_radix(&("11100000".repeat(8)).as_str(), 2).unwrap(), (true, "▍"));
+    transforms.insert(u64::from_str_radix(&("11000000".repeat(8)).as_str(), 2).unwrap(), (true, "▎"));
+    transforms.insert(u64::from_str_radix(&("10000000".repeat(8)).as_str(), 2).unwrap(), (true, "▏"));
+    transforms.insert(u64::from_str_radix(&("00001111".repeat(8)).as_str(), 2).unwrap(), (true, "▐"));
+    transforms.insert(u64::from_str_radix(&(("1000100000100010").repeat(4)).as_str(), 2).unwrap(), (true, "░"));
+    transforms.insert(u64::from_str_radix(&(("1010101001010100").repeat(4)).as_str(), 2).unwrap(), (true, "▒"));
+    transforms.insert(u64::from_str_radix(&(("0111011111011101").repeat(4)).as_str(), 2).unwrap(), (true, "▓"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8)+&"0".repeat(8).repeat(7)).as_str(), 2).unwrap(), (true, "▔"));
+    transforms.insert(u64::from_str_radix(&("00000001".repeat(8)).as_str(), 2).unwrap(), (true, "▕"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(4)+&"11110000".repeat(4)).as_str(), 2).unwrap(), (true, "▖"));
+    transforms.insert(u64::from_str_radix(&("0".repeat(8).repeat(4)+&"00001111".repeat(4)).as_str(), 2).unwrap(), (true, "▗"));
+    transforms.insert(u64::from_str_radix(&("11110000".repeat(4)+&"0".repeat(8).repeat(4)).as_str(), 2).unwrap(), (true, "▘"));
+    transforms.insert(u64::from_str_radix(&("11110000".repeat(4)+&"1".repeat(8).repeat(4)).as_str(), 2).unwrap(), (true, "▙"));
+    transforms.insert(u64::from_str_radix(&("11110000".repeat(4)+&"00001111".repeat(4)).as_str(), 2).unwrap(), (true, "▚"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(4)+&"11110000".repeat(4)).as_str(), 2).unwrap(), (true, "▛"));
+    transforms.insert(u64::from_str_radix(&("1".repeat(8).repeat(4)+&"00001111".repeat(4)).as_str(), 2).unwrap(), (true, "▜"));
+    transforms.insert(u64::from_str_radix(&("00001111".repeat(4)+&"0".repeat(8).repeat(4)).as_str(), 2).unwrap(), (true, "▝"));
+    transforms.insert(u64::from_str_radix(&("00001111".repeat(4)+&"11110000".repeat(4)).as_str(), 2).unwrap(), (true, "▞"));
+    transforms.insert(u64::from_str_radix(&("00001111".repeat(4)+&"1".repeat(8).repeat(4)).as_str(), 2).unwrap(), (true, "▟"));
+
+    let mut transforms_keys : Vec<u64> = Vec::new();
+    for k in transforms.keys() {
+        transforms_keys.push(*k);
+    }
+    const AVERAGE_SIZE: usize = 8;
+    let mut sorted: [(usize, usize, (u8, u8, u8)); AVERAGE_SIZE] = [(0, 0, (0, 0, 0)); AVERAGE_SIZE];
+    let mut grey_scales_start: [usize; 32] = [0; 32];
+    let mut grey_scales_end: [usize; 32] = [0; 32];
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write_all(format!("\x1b[{};0f", 0).as_bytes()).unwrap();
+    for y in 0..(height / 16) {
+        let mut x = 0;
+        while x < (width / 8) {
+           let mut sum_grey_scale : usize = 0;
+           let mut i = 0;
+           let mut dy : usize = 0;
+           let mut dx : usize;
+           while dy < 8 {
+               dx = 0;
+               while dx < 8 {
+                   let _x = x * 8 + (dx as u32);
+                   let _y = y * 16 + (dy as u32) * 2;
+                   let block = coordinate_to_rgb(_x, _y);
+                   let grey = grey_scale(&block);
+                   /* do not write every pixel in sorted so that sort_by is faster
+                    * the downside is that this reduce quality a lot */
+                   if i % AVERAGE_SIZE == dy { sorted[dy] = (grey, i, block) };
+                   if i < 32 {
+                       grey_scales_start[i] = grey;
+                   }
+                   else {
+                       grey_scales_end[i - 32] = grey;
+                   }
+                   sum_grey_scale += grey;
+                   i += 1;
+                   dx += 1;
+               }
+               dy += 1
+           }
+           let average_grey_scale : usize = sum_grey_scale / 64;
+           sorted.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+           let average_min = sorted[AVERAGE_SIZE / 4].2;
+           let average_max = sorted[(3 * AVERAGE_SIZE) / 4].2;
+           let mut group = 0;
+           for grey in &grey_scales_start {
+               group = group << 1 | (if grey >= &average_grey_scale { 1 } else { 0 });
+           }
+           for grey in &grey_scales_end {
+               group = group << 1 | (if grey >= &average_grey_scale { 1 } else { 0 });
+           }
+           let transform = match transforms.get(&group) {
+               Some(t) => t,
+               _ => {
+                   let closest = find_closest_group(&transforms_keys, group);
+                   match transforms.get(&closest) {
+                       Some(x) => {
+                           x
+                       },
+                       _ => &(true, " ")
+                   }
+                }
+           }.clone();
+           let fg = if transform.0 { average_max } else { average_min };
+           let bg = if transform.0 { average_min } else { average_max };
+           let result = transform.1;
+           handle.write_all(format!("\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m{}", fg.0, fg.1, fg.2, bg.0, bg.1, bg.2, result).as_bytes()).unwrap();
+           x += 1;
+        }
+        handle.write_all(b"\x1b[0m\n").unwrap();
+    }
+}
